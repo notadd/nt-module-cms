3e1ca8f25a0cabced0bfa37275f606d7416b820a
ykxhzfj <576118533@qq.com>
2018-11-16 14:47:29 +0800

    chore(cms): change config

--- src/cms.module.ts
+++ src/cms.module.ts
@@ -1,7 +1,31 @@
 import { Module } from "@nestjs/common";
+import { ArticleController } from "./controllers/article.controller";
+import { ClassifyController } from "./controllers/classify.controller";
+import { MessageController } from "./controllers/message.controller";
+import { ArticleService } from "./services/article.service";
+import { MessageService } from "./services/message.service";
+import { ClassifyService } from "./services/classify.service";
+import { ClassifyEntity } from "./entities/classify.entity";
+import { MessageEntity } from "./entities/message.entity";
+import { ArticleEntity } from "./entities/article.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-    controllers: [],
-    providers: []
+    imports: [
+        TypeOrmModule.forRoot({
+            
+        }),
+        TypeOrmModule.forFeature([ClassifyEntity,MessageEntity,ArticleEntity,]),
+    ],
+    controllers: [
+        ArticleController,
+        ClassifyController,
+        MessageController,
+    ],
+    providers: [
+        ArticleService,
+        MessageService,
+        ClassifyService,
+    ]
 })
 export class CmsModule { }
