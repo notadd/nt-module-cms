4d667f88eeaccd08acaad808337ddda36be2a310
ykxhzfj <576118533@qq.com>
2018-12-06 19:37:36 +0800

    chore(cms): add moduleInit

--- src/cms.module.ts
+++ src/cms.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Module, OnModuleInit } from "@nestjs/common";
 import { ArticleController } from "./controllers/article.controller";
 import { ClassifyController } from "./controllers/classify.controller";
 import { MessageController } from "./controllers/message.controller";
@@ -8,15 +8,16 @@ import { ClassifyService } from "./services/classify.service";
 import { ClassifyEntity } from "./entities/classify.entity";
 import { MessageEntity } from "./entities/message.entity";
 import { ArticleEntity } from "./entities/article.entity";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule, InjectRepository } from "@nestjs/typeorm";
 import { NotaddGrpcClientFactory } from "./grpc.client-factory";
+import { TreeRepository } from "typeorm";
 
 @Module({
     imports: [
         TypeOrmModule.forRoot({
-            
+
         }),
-        TypeOrmModule.forFeature([ClassifyEntity,MessageEntity,ArticleEntity,]),
+        TypeOrmModule.forFeature([ClassifyEntity, MessageEntity, ArticleEntity,]),
     ],
     controllers: [
         ArticleController,
@@ -30,4 +31,21 @@ import { NotaddGrpcClientFactory } from "./grpc.client-factory";
         NotaddGrpcClientFactory
     ]
 })
-export class CmsModule { }
+
+export class CmsModule implements OnModuleInit {
+    constructor(
+        @InjectRepository(ClassifyEntity) private readonly claRepository: TreeRepository<ClassifyEntity>,
+        private readonly classifyService: ClassifyService,
+    ) { }
+
+    async onModuleInit() {
+        await this.createRootClassify();
+    }
+
+    private async createRootClassify() {
+        const root = await this.claRepository.findOne({ where: { title: '总分类' } });
+        if (!root) {
+            await this.classifyService.addClassify({ name: '总分类', parent: { id: 0 } });
+        }
+    }
+}
