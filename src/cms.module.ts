d0eb9b2d899ea280892c5dee0f81b5d09e8fad44
ykxhzfj <576118533@qq.com>
2018-12-06 17:48:53 +0800

    feat(cmsModule): add UserModule

--- src/cms.module.ts
+++ src/cms.module.ts
@@ -9,6 +9,7 @@ import { ClassifyEntity } from "./entities/classify.entity";
 import { MessageEntity } from "./entities/message.entity";
 import { ArticleEntity } from "./entities/article.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { NotaddGrpcClientFactory } from "./grpc.client-factory";
 
 @Module({
     imports: [
@@ -26,6 +27,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
         ArticleService,
         MessageService,
         ClassifyService,
+        NotaddGrpcClientFactory
     ]
 })
 export class CmsModule { }
---
+++ src/grpc.client-factory.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Client, ClientGrpc, GrpcOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+@Injectable()
+export class NotaddGrpcClientFactory {
+    @Client(generateGrpcOptions('localhost:50051', 'notadd_module_user', 'user-module.proto'))
+    public readonly userModuleClient: ClientGrpc;
+}
+
+export function generateGrpcOptions(url: string, packageName: string, protoFileName: string): GrpcOptions {
+    return {
+        transport: Transport.GRPC,
+        options: {
+            url,
+            package: packageName,
+            protoPath: join(__dirname, 'protobufs/' + protoFileName),
+            loader: {
+                arrays: true
+            }
+        }
+    };
+}
