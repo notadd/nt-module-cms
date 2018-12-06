b7b686c136698ad67dffb1361d4e722ce0250f3c
ykxhzfj <576118533@qq.com>
2018-12-06 19:36:42 +0800

    chore(message): delete useless service

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -15,7 +15,7 @@ export class MessageController {
     }
 
     @GrpcMethod('MessageService')
-    async deleteMessageById(body: { ids: number[] }) {
+    async deleteMessageByIds(body: { ids: number[] }) {
         await this.messageService.deleteMessageById(body.ids);
         return { code: 200, message: '删除成功!' };
     }
@@ -24,13 +24,6 @@ export class MessageController {
     async getAllMessage(body: { pageNumber: number, pageSize: number, startTime: string, endTime: string }) {
         const data = (await this.messageService.getAllMessage(body.pageNumber, body.pageSize, body.startTime, body.endTime)).data;
         const total = (await this.messageService.getAllMessage(body.pageNumber, body.pageSize, body.startTime, body.endTime)).total;
-        return { code: 200, message: '查询成功!', data, total };
-    }
-
-    @GrpcMethod('MessageService')
-    async getMessageByUserId(body:{ pageNumber: number, pageSize: number, startTime: string, endTime: string ,id:number}){
-        const data = (await this.messageService.getMessageByUserId(body.pageNumber, body.pageSize, body.startTime, body.endTime,body.id)).data;
-        const total = (await this.messageService.getMessageByUserId(body.pageNumber, body.pageSize, body.startTime, body.endTime,body.id)).total;
         return { code: 200, message: '查询成功!', data, total };
     }
 
