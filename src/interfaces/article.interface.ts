eb21aaebd2fe5fabc83e78a5ccffd02f4a518973
ykxhzfj <576118533@qq.com>
2018-12-07 11:49:06 +0800

    feat(article): add interface

--- src/interfaces/article.interface.ts
+++ src/interfaces/article.interface.ts
@@ -16,8 +16,23 @@ export interface updateArticle {
     classifyId: number;
     sourceUrl: string;
     cover: string;
-    asbstact: string;
+    abstract: string;
     content: string;
     top: boolean;
     source: string;
+}
+
+export  interface artResult {
+    id: number;
+    title: string;
+    classifyId: number;
+    classifyName: string;
+    sourceUrl: string;
+    cover: string;
+    abstract: string;
+    content: string;
+    top: boolean;
+    source: string;
+    userId: number;
+    userName: string;
 }
---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,63 @@
+export interface UserInfoData {
+    id: number;
+    username: string;
+    email: string;
+    mobile: string;
+    banned: boolean;
+    recycle: boolean;
+    createTime: string;
+    updateTime: string;
+    status: number;
+    certification: number;
+    userRoles: {
+        id: number;
+        name: string
+    }[];
+    userOrganizations: {
+        id: number;
+        name: string;
+    }[];
+    userInfos: {
+        id: number;
+        order: number;
+        relationId: number;
+        type: string;
+        name: string;
+        value: string;
+        description: string;
+        registerDisplay: boolean;
+        informationDisplay: boolean;
+    }[];
+}
+
+export interface CreateUserInput {
+    username?: string;
+    email?: string;
+    mobile?: string;
+    password: string;
+    infoKVs?: { key: number; value: string }[];
+    roleIds?: number[];
+    organizationIds?: number[];
+}
+
+export interface UpdateUserInput {
+    username?: string;
+    email?: string;
+    mobile?: string;
+    password?: string;
+    status: number;
+    certification: number;
+    infoKVs?: {
+        key: number;
+        value: string;
+        relationId?: number
+    }[];
+    roleIds?: {
+        before: number;
+        after: number;
+    }[];
+    organizationIds?: {
+        before: number;
+        after: number;
+    }[];
+}
